86153045410e8fa8d9bca5a10cf8928c4ca87c81
John Kelly <jvkelly.jk@gmail.com>
2026-01-11 15:33:15 -0600

    remove unused elements

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -4,13 +4,9 @@ namespace controller;
 
 use core\Controller;
 use Scratchy\component\PageContent;
-use Scratchy\component\TextInput;
-use Scratchy\elements\button;
 use Scratchy\elements\Element;
-use Scratchy\elements\form;
 use Scratchy\elements\h1;
 use Scratchy\elements\h2;
-use Scratchy\elements\p;
 
 class HomeController extends Controller
 {
