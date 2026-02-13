df76e525b70a8f6a161089f9192e7ef8ddc806b9
John Kelly <jvkelly.jk@gmail.com>
2026-02-13 10:21:56 -0600

    clean up tags

--- src/Scratchy/elements/source.php
+++ src/Scratchy/elements/source.php
@@ -8,17 +8,12 @@ use Scratchy\TagType;
 class source extends Element
 {
     public function __construct(
-        private ?string $name = null,
-        private ?string $value = null,
         private ?string $id = null,
         private ?array  $classes = [],
         private ?array  $attributes = [],
-        private ?string $content = null,
     )
     {
-        $this->attributes['name'] = $this->name ??= ($this->id ?? $this->makeUniqueName());
-        $this->attributes['value'] = $this->value ?? $this->getInputDefault($this->name);
-        parent::__construct(TagType::source, $this->id, $this->classes, $this->attributes, $this->content, false);
+        parent::__construct(TagType::source, $this->id, $this->classes, $this->attributes, null, false);
     }
 
     /**
@@ -26,6 +21,6 @@ class source extends Element
      */
     public function append(...$args): void
     {
-        throw new Exception('You may not append an element to input tags');
+        throw new Exception('You may not append an element to source tags');
     }
 }
